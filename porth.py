5b9470851fd38783dc06e74bfcf6cd85f4ab3310
Shilin Victor <chrono.monochrome@gmail.com>
2021-12-02 14:55:21 +0300

    ARM: insert LTORG assembly instructions for bigger files to compile

--- porth.py
+++ porth.py
@@ -1481,6 +1481,7 @@ def generate_nasm_linux_arm32(program: Program, out_file_path: str):
                 out.write("    push {r1}\n")
                 out.write("    ldr r1, =str_%d\n" % len(strs))
                 out.write("    push {r1}\n")
+                out.write("    .ltorg\n")
                 strs.append(value)
             elif op.typ == OpType.PUSH_CSTR:
                 out.write("// PUSH_CSTR\n")
@@ -1488,6 +1489,7 @@ def generate_nasm_linux_arm32(program: Program, out_file_path: str):
                 value = op.operand.encode('utf-8') + b'\0'
                 out.write("    ldr r1, =str_%d\n" % len(strs))
                 out.write("    push {r1}\n")
+                out.write("    .ltorg\n")
                 strs.append(value)
             elif op.typ == OpType.PUSH_MEM:
                 out.write("// PUSH_MEM\n")
@@ -1495,6 +1497,7 @@ def generate_nasm_linux_arm32(program: Program, out_file_path: str):
                 out.write("    ldr r1, =mem\n")
                 out.write("    add r1, #%d\n" % (op.operand & 0xff))
                 out.write("    push {r1}\n")
+                out.write("    .ltorg\n")
             elif op.typ == OpType.PUSH_LOCAL_MEM:
                 out.write("// PUSH_LOCAL_MEM\n")
                 assert isinstance(op.operand, MemAddr)
@@ -1536,6 +1539,7 @@ def generate_nasm_linux_arm32(program: Program, out_file_path: str):
                 out.write("    str lr, [r1]\n")
                 out.write("    add r3, r3, #4\n")
                 out.write("    str r3, [r2]\n")
+                out.write("    .ltorg\n")
                 assert isinstance(op.operand, int)
                 # out.write("    sub rsp, %d\n" % op.operand)
                 # out.write("    mov [ret_stack_rsp], rsp\n")
